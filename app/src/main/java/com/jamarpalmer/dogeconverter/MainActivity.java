1b2fe8d1d26cb58eb1b20cdeb6501951bdd212c3
Jamar <64226908+jamar-palmer@users.noreply.github.com>
2021-06-21 11:20:42 -0400

    init setup

---
+++ app/src/main/java/com/jamarpalmer/dogeconverter/MainActivity.java
@@ -0,0 +1,20 @@
+package com.jamarpalmer.dogeconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //eventual
+        //swap button to reverse entry
+        //get doge meme pics
+        //regularly updated
+    }
+}
